1b1cc62d7e36bcbb1db3b777c38f824808828dc6
Ben Camilleri <bencamilleri3@gmail.com>
2025-12-03 20:37:20 +0100

    Modify timer reset and reps display logic
    Update the reset_timer function to initialize reps to -1 and display the current reps count in the UI.

--- main.py
+++ main.py
@@ -17,10 +17,12 @@ timer = None
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
+    canvas.itemconfig(reps_text, text="Reps: 0")
     title_label.config(text="Timer")
     check_marks.config(text="")
     global reps
-    reps = 0
+    reps = -1
+
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -28,6 +30,7 @@ def reset_timer():
 def start_timer():
     global reps
     reps += 1
+    canvas.itemconfig(reps_text, text=f"Reps: {reps}") 
 
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
@@ -59,13 +62,17 @@ def count_down(count):
         global timer
         timer = window.after(1000, count_down, count - 1)
     else:
+        # Update reps ONLY after a work session ends
+        if reps % 2 == 1:
+            canvas.itemconfig(reps_text, text=f"Reps: {math.floor(reps/2)}")
+
         start_timer()
+        
         marks = ""
         work_sessions = math.floor(reps/2)
         for _ in range(work_sessions):
             marks += "✔"
         check_marks.config(text=marks)
-
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -80,6 +87,8 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+reps_text = canvas.create_text(100, 200, text="Reps: 0", fill="black", font=(FONT_NAME, 15, "bold"))
+
 canvas.grid(column=1, row=1)
 
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
@@ -91,6 +100,7 @@ reset_button.grid(column=2, row=2)
 check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
+#repscheck_marks = Label()
 
 
 
